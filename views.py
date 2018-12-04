73fe6d08de220b7ea595f190f16298f70e364817
Scott Chen <scottchen@haizol.com>
2018-12-04 17:23:06 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django import render
 
 def index(request):
     return HttpResponse('Helllo World')
+
+def login(request):
+    return render(request, 'app/login.html')
